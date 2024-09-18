9c296c1263a485d03594733d5abaa2e979668315
rtk0c <mail.tianyig@gmail.com>
2024-09-18 09:19:26 -0700

    FIFO and socket data transport, quick'n'dirty

--- sockbridge/src/main.c
+++ sockbridge/src/main.c
@@ -1,18 +1,22 @@
 #include "arena.h"
 #include "str.h"
 
-#include <string.h>
-#include <stdlib.h>
+#include <asm-generic/ioctls.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <errno.h>
 #include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <netinet/in.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <sys/epoll.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static void ensure_fifo_exists(const char* path, const char* fifo_usage) {
     struct stat sb;
@@ -77,24 +81,9 @@ static int socket_to_upstream_debugger(const char* upstream_host, const char* up
         exit(EXIT_FAILURE);
     }
 
+    ioctl(sockfd, FIONBIO);
+
     return sockfd;
-}
-
-typedef struct {
-    const char* fifo_path;
-
-    int result_fd;
-} FifoOpener;
-
-static void* fifo_opener_thrd_func(void* data_) {
-    FifoOpener* data = (FifoOpener*)data_;
-
-    int fd = open(data->fifo_path, 0);
-    if (fd == -1) {
-
-    }
-
-    return NULL;
 }
 
 static int open_game2dbg_fifo(const char* path) {
@@ -116,7 +105,32 @@ static int open_dbg2game_fifo(const char* path) {
         exit(EXIT_FAILURE);
     }
 
+    // And then set it to nonblocking, so we can use it with epoll
+    int flags = fcntl(fd, F_GETFL, 0);
+    flags |= O_NONBLOCK;
+    fcntl(fd, F_SETFL, flags);
+
     return fd;
+}
+
+static int setup_epoll() {
+    int epollfd = epoll_create1(0);
+    if (epollfd == -1) {
+        perror("Failed to create epoll");
+        exit(EXIT_FAILURE);
+    }
+
+    return epollfd;
+}
+
+static void add_epoll_fd(int epollfd, int flags, int watched_fd) {
+    struct epoll_event ev;
+    ev.events = flags;
+    ev.data.fd = watched_fd;
+    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, watched_fd, &ev) == -1) {
+        perror("Failed to add epoll watched fd");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char** argv) {
@@ -145,13 +159,62 @@ int main(int argc, char** argv) {
     int game2dbg_fd = open_game2dbg_fifo(game2dbg_path);
     int dbg2game_fd = open_dbg2game_fifo(dbg2game_path);
     printf("Connected.\n");
-  
+
     printf("Connecting to upstream %s:%s...\n", upstream_host, upstream_port);
     int sockfd = socket_to_upstream_debugger(upstream_host, upstream_port);
     printf("Connected.\n");
 
-    // TODO pipe data
+    int epollfd = setup_epoll();
+    add_epoll_fd(epollfd, EPOLLIN, sockfd);
+    add_epoll_fd(epollfd, EPOLLIN, game2dbg_fd);
+    // TODO do not assume writes will finish in one go
+    // add_epoll_fd(epollfd, EPOLLOUT, dbg2game_fd);
 
+#define MAX_EVENTS 8
+
+    char dbg2game_buffer[2048];
+    char game2dbg_buffer[2048];
+
+    for (;;) {
+        struct epoll_event events[MAX_EVENTS];
+        int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+        if (nfds == -1) {
+            perror("Failed to wait on epoll");
+            exit(EXIT_FAILURE);
+        }
+
+        for (int i = 0; i < nfds; ++i) {
+            int thefd = events[i].data.fd;
+            if (thefd == sockfd) {
+                int n = recv(sockfd, dbg2game_buffer, sizeof(dbg2game_buffer), 0);
+                if (n == 0)
+                    goto exit;
+                else if (n == -1)
+                    // handle_recv_error(sockfd, error);
+                    ;
+
+                int res = write(dbg2game_fd, dbg2game_buffer, n);
+                if (res == -1) {
+                }
+            } else if (thefd == game2dbg_fd) {
+                int n = read(game2dbg_fd, game2dbg_buffer, sizeof(game2dbg_buffer));
+                if (n == 0)
+                    goto exit;
+                else if (n == -1)
+                    // handle_recv_error(sockfd, error);
+                    ;
+                
+                int res = send(sockfd, game2dbg_buffer, n, 0);
+                if (res == -1) {
+                }
+            } else if (thefd == dbg2game_fd) {
+                // TODO write extra data
+            }
+        }
+    }
+
+exit:
     // Let's not free `a`. We're exiting anyways.
+    // Or any of the other resources
     return 0;
 }
